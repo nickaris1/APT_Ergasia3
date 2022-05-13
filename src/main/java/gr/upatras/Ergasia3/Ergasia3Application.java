01568a3f05ca84f032e1121bce665bf9b0d60ad2
nickaris1 <issarisnick@outlook.com>
2022-05-13 18:24:37 +0300

    init

---
+++ src/main/java/gr/upatras/Ergasia3/Ergasia3Application.java
@@ -0,0 +1,13 @@
+package gr.upatras.Ergasia3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ergasia3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ergasia3Application.class, args);
+	}
+
+}
